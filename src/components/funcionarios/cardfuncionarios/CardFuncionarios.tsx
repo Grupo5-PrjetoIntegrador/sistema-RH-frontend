3b972aa22e018d6521ca09c1593940279c27c7d4
annavighi <vighianna@gmail.com>
2025-01-31 17:41:08 -0300

    bugs fixed

--- src/components/funcionarios/cardfuncionarios/CardFuncionarios.tsx
+++ src/components/funcionarios/cardfuncionarios/CardFuncionarios.tsx
@@ -2,43 +2,43 @@ import { Link } from 'react-router-dom'
 import Funcionario from '../../../models/Funcionario'
 
 interface CardFuncionariosProps {
-    postagem: Funcionario
+    funcionario: Funcionario
 }
 
-function CardPostagem({ postagem }: CardFuncionariosProps) {
+function CardFuncionario({ funcionario }: CardFuncionariosProps) {
     return (
         <div className='border-slate-900 border 
             flex flex-col rounded overflow-hidden justify-between'>
-                
+
             <div>
                 <div className="flex w-full bg-indigo-400 py-2 px-4 items-center gap-4">
                     <img
-                        src={postagem.usuario?.foto}
+                        src={funcionario.usuario?.foto}
                         className='h-12 rounded-full'
-                        alt={postagem.usuario?.nome} />
+                        alt={funcionario.usuario?.nome} />
                     <h3 className='text-lg font-bold text-center uppercase'>
-                        {postagem.usuario?.nome}
+                        {funcionario.usuario?.nome}
                     </h3>
                 </div>
                 <div className='p-4 '>
-                    <h4 className='text-lg font-semibold uppercase'>{postagem.titulo}</h4>
-                    <p>{postagem.texto}</p>
-                    <p>Tema: {postagem.tema?.descricao}</p>
+                    <h4 className='text-lg font-semibold uppercase'>{funcionario.nome}</h4>
+                    <p>{funcionario.texto}</p>
+                    <p>Tema: {funcionario.tema?.nomeSetor}</p>
                     <p>Data: {new Intl.DateTimeFormat(undefined, {
                         dateStyle: 'full',
                         timeStyle: 'medium',
-                    }).format(new Date(postagem.data))}</p>
+                    }).format(new Date(funcionario.data))}</p>
                 </div>
             </div>
             <div className="flex">
-                <Link to='' 
+                <Link to=''
                     className='w-full text-white bg-indigo-400 
                     hover:bg-indigo-800 flex items-center justify-center py-2'>
                     <button>Editar</button>
                 </Link>
-                <Link to='' 
+                <Link to={`/deletarpostagem/${funcionario.id}`}
                     className='text-white bg-red-400 
-                    hover:bg-red-700 w-full flex items-center justify-center'>
+                	hover:bg-red-700 w-full flex items-center justify-center'>
                     <button>Deletar</button>
                 </Link>
             </div>
@@ -46,4 +46,4 @@ function CardPostagem({ postagem }: CardFuncionariosProps) {
     )
 }
 
-export default CardPostagem
+export default CardFuncionario
--- src/models/Funcionario.ts
+++ src/models/Funcionario.ts
@@ -1,3 +1,4 @@
+import { ReactNode } from 'react';
 import Setor from './Setor';
 import Usuario from './Usuario';
 

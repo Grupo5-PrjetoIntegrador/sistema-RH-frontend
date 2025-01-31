0516183e2d4b53707e47792bbd92134025938cb0
CaciaRodrigues <caciarodriguues@gmail.com>
2025-01-31 11:18:31 -0300

    Modelagem do componente Funcionário

---
+++ src/models/Funcionario.ts
@@ -0,0 +1,15 @@
+import Setor from './Setor';
+import Usuario from './Usuario';
+
+export default interface Funcionario {
+    id: number;
+    nome: string;
+    cargo: string;
+    salarioBase: number;
+    horasTrabalhadas: number;
+    bonus: number;
+    descontos: number;
+    aniversarioEmpresa: string;
+    setor: Setor | null;
+    usuario: Usuario | null;
+}

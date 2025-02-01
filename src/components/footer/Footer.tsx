3520a6afb62c8506abd305b25ea92a2861d33d14
CaciaRodrigues <caciarodriguues@gmail.com>
2025-02-01 14:10:17 -0300

    Estilizando o footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,4 +1,4 @@
-import { FacebookLogo, InstagramLogo, LinkedinLogo, GithubLogo } from '@phosphor-icons/react'
+import { GithubLogo } from '@phosphor-icons/react'
 
 function Footer() {
 
@@ -6,21 +6,17 @@ function Footer() {
 
     return (
         <>
-            <div className="flex justify-center bg-indigo-900 text-white">
-                <div className="container flex flex-col items-center py-4">
-                    <p className='text-xl font-bold'>
-                            Sistema RH | Copyright: {data}
-                        </p>
-                    <p className='text-lg'>Acesse nossas redes sociais</p>
-                    <div className='flex gap-2'>
-                        <LinkedinLogo size={48} weight='bold' />
-                        <InstagramLogo size={48} weight='bold' />
-                        <FacebookLogo size={48} weight='bold' />
-                        <a href="https://github.com/Grupo5-PrjetoIntegrador" target="_blank" rel="noopener noreferrer" className="hover:text-teal-400">
-                            <GithubLogo size={48} weight="bold" />
-                        </a>
-
-                    </div>
+            <div className="flex justify-between items-center bg-blue-950 text-blue-50 py-2">
+                <div className='w-1/3 flex gap-2 items-center justify-center py-4'>
+                <p className='text-lg'>Veja mais projetos:</p>
+                <a href="https://github.com/Grupo5-PrjetoIntegrador" target="_blank" rel="noopener noreferrer" className="hover:text-cor-destaque">
+                <GithubLogo size={34} weight="bold" /></a>
+                    
+                    <p> e conheca os <span className='text-cor-destaque'>Desenvolvedores</span></p>
+                </div>
+                <div className="container flex  flex-col items-center w-1/6">
+                    <p className='text-xl font-semibold'>Build Minds RH &#169; {data}</p>
+                   
                 </div>
             </div>
         </>

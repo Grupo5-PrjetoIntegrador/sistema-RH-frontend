1605f7287148a72bffe8a893a650a6ef206da651
CaciaRodrigues <caciarodriguues@gmail.com>
2025-02-03 10:41:50 -0300

    Adicionando Informações Bruno e Marcos

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -9,7 +9,6 @@ function Home() {
                         <h2 className="text-5xl font-bold">Build Minds RH</h2>
                         <p className="text-xl">Gestão inteligente para equipes de alto desempenho.</p>
 
-                        {/* Logo com Link para rolar até a seção "Sobre Nós" */}
                         <a href="#sobre-nos">
                             <ArrowDownCircleIcon aria-hidden="true" className="size-16 text-blue-950/75 hover:text-blue-800" />
                         </a>
@@ -25,7 +24,6 @@ function Home() {
                 </div>
             </div>
 
-            {/* Seção "Sobre Nós" com ID para ser referenciada */}
             <section id="sobre-nos" className="bg-blue-50 py-32 w-full">
                 <div className="container mx-auto text-center px-32">
                     <h3 className="text-3xl font-bold text-blue-950 mb-4">Sobre Nós</h3>
--- src/utils/DevData.tsx
+++ src/utils/DevData.tsx
@@ -27,29 +27,24 @@ export const pessoas = [
   },
   {
     nome: "Marcos Lopes",
-    image:
-      "https://ik.imagekit.io/caciasrs/PI%20-%20Gen%20RH/cat-svgrepo-com.svg?updatedAt=1738345536670",
+    image: "https://ik.imagekit.io/caciasrs/PI%20-%20Gen%20RH/cat-svgrepo-com.svg?updatedAt=1738345536670",
     github: "https://github.com/marcoslopesdev",
     linkedin: "https://www.linkedin.com/in/marcosaflopes/",
-    email: "email@email.com",
-    titulo: "Desenvolvedor (Insira a Stack)",
-    tecnologias:
-      "Blah, BlahBlah, BlahBlah, BlahBlah, BlahBlah, BlahBlah, BlahBlah, BlahBlah, BlahBlah",
-      descricao: "Doloribus dolores nostrum quia qui natus officia quod et dolorem. Sit repellendus qui ut at blanditiis et quo et molestiae.",
-    
-      funfact: "Este é um fato divertido sobre mim",
+    email: "99marcoslopes@gmail.com",
+    titulo: "Desenvolvedor Java FullStack",
+    tecnologias: "Java, Spring Boot, MySQL, React, TypeScript, Node",
+    descricao: "Sou apaixonado por tecnologia e por criar soluções que tornam a vida das pessoas mais fácil. No tempo livre, gosto de ler, ver filmes, estar em contato com a natureza e aproveitar momentos com quem amo.",
+    funfact: "Toco saxofone e acompanho todas as corridas de Fórmula 1.",
   },
   {
     nome: "Bruno Alexandre",
-    image:
-      "https://ik.imagekit.io/caciasrs/PI%20-%20Gen%20RH/cat-svgrepo-com.svg?updatedAt=1738345536670",
+    image: "https://ik.imagekit.io/caciasrs/PI%20-%20Gen%20RH/cat-svgrepo-com.svg?updatedAt=1738345536670",
     github: "https://github.com/brunoalexb",
     linkedin: "https://www.linkedin.com/in/brunoa-barros/",
-    email: "email@email.com",
-    titulo: "Desenvolvedor (Insira a Stack)",
-    tecnologias:
-      "Blah, BlahBlah, BlahBlah, BlahBlah, BlahBlah, BlahBlah, BlahBlah, BlahBlah, BlahBlah",
-      descricao: "Doloribus dolores nostrum quia qui natus officia quod et dolorem. Sit repellendus qui ut at blanditiis et quo et molestiae.",
-    funfact: "Este é um fato divertido sobre mim",
+    email: "brunoale77@gmail.com",
+    titulo: "Desenvolvedor FullStack",
+    tecnologias: "Java, Spring Boot, JavaScript, Node.js, TypeScript, React, Python e  MySQL",
+    descricao: "Desenvolvedor fullstack que atua na criação de sistemas completos, APIs seguras e soluções inovadoras. Apaixonado por tecnologia, inovação e aprendizado contínuo.",
+    funfact: "Tenho formação em História.",
   },
 ];
